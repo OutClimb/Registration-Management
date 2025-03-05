362ac01f8e151433d46b9f2dc55a6d17e9090fe6
Nickel Mattera <me@nicholemattera.gay>
2025-03-05 17:34:38 -0600

    fix: error handling in utils

--- src/utils/form.ts
+++ src/utils/form.ts
@@ -2,38 +2,52 @@ import { getBaseURL } from './env'
 import type { FormDetailResponse, FormResponse } from '@/types/form'
 
 export async function fetchForm(slug: string): Promise<FormDetailResponse> {
+  let response;
+
   try {
-    const response = await fetch(`${getBaseURL()}/api/v1/form/${slug}`, {
+    response = await fetch(`${getBaseURL()}/api/v1/form/${slug}`, {
       method: 'GET',
     })
-    if (response.status === 404) {
-      throw new Error('Not Found')
-    } else if (response.status >= 300) {
-      throw new Error('An error occurred. Please try again.')
-    } else {
-      return await response.json()
-    }
+  } catch {
+    throw new Error('An error occurred. Please try again.')
+  }
+
+  if (response.status === 404) {
+    throw new Error('Not Found')
+  } else if (!response.ok) {
+    throw new Error('An error occurred. Please try again.')
+  }
+  
+  try {
+    return await response.json()
   } catch {
     throw new Error('An error occurred. Please try again.')
   }
 }
 
 export async function fetchForms(): Promise<FormResponse> {
+  const token = localStorage.getItem('token') || ''
+  let response;
+
   try {
-    const token = localStorage.getItem('token') || ''
-    const response = await fetch(`${getBaseURL()}/api/v1/form`, {
+    response = await fetch(`${getBaseURL()}/api/v1/form`, {
       method: 'GET',
       headers: {
         Authorization: token,
       },
     })
-    if (response.status === 401) {
-      throw new Error('Unauthorized')
-    } else if (response.status >= 300) {
-      throw new Error('An error occurred. Please try again.')
-    } else {
-      return await response.json()
-    }
+  } catch {
+    throw new Error('An error occurred. Please try again.')
+  }
+
+  if (response.status === 401) {
+    throw new Error('Unauthorized')
+  } else if (!response.ok) {
+    throw new Error('An error occurred. Please try again.')
+  }
+
+  try {
+    return await response.json()
   } catch {
     throw new Error('An error occurred. Please try again.')
   }
--- src/utils/submission.ts
+++ src/utils/submission.ts
@@ -2,23 +2,30 @@ import type { SubmissionResponse } from '@/types/submission'
 import { getBaseURL } from './env'
 
 export async function fetchSubmissions(formSlug: string): Promise<SubmissionResponse> {
+  const token = localStorage.getItem('token') || ''
+  let response;
+
   try {
-    const token = localStorage.getItem('token') || ''
-    const response = await fetch(`${getBaseURL()}/api/v1/submission/${formSlug}`, {
+    response = await fetch(`${getBaseURL()}/api/v1/submission/${formSlug}`, {
       method: 'GET',
       headers: {
         Authorization: token,
       },
     })
-    if (response.status === 401) {
-      throw new Error('Unauthorized')
-    } else if (response.status === 404) {
-      throw new Error('Not Found')
-    } else if (response.status >= 300) {
-      throw new Error('An error occurred. Please try again.')
-    } else {
-      return await response.json()
-    }
+  } catch {
+    throw new Error('An error occurred. Please try again.')
+  }
+
+  if (response.status === 401) {
+    throw new Error('Unauthorized')
+  } else if (response.status === 404) {
+    throw new Error('Not Found')
+  } else if (!response.ok) {
+    throw new Error('An error occurred. Please try again.')
+  }
+
+  try {
+    return await response.json()
   } catch {
     throw new Error('An error occurred. Please try again.')
   }
